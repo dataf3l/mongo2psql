8ad70d29d04495239a0344bd9e15807f5fcd6038
Felipe@MBP2 <auditor400@gmail.com>
2018-07-11 11:46:28 -0500

    Example table

---
+++ db/messages2.sql
@@ -0,0 +1,17 @@
+
+create table lead_source (
+    _id varchar(100),
+    first_name varchar(50),
+    last_name varchar(50),
+    email varchar(255),
+    phone_number varchar(20),
+    source varchar(20),
+    apply_date timestamp,
+    message_id varchar(100),
+    submitted_to_vipkid integer
+)
+
+-- INSERT INTO lead_source (_id,first_name,last_name,email,phone_number,source,apply_date,message_id,submitted_to_vipkid)
+-- VALUES('1','1','1','1','1','1','2018-01-01','1','0');
+
+
