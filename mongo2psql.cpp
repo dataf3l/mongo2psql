6289b45b48bac20879cee6169e356423d7d1a446
Felipe@MBP2 <auditor400@gmail.com>
2018-07-11 10:54:40 -0500

    mongo 2 psql initial

---
+++ mongo2psql.cpp
@@ -0,0 +1,19 @@
+/**
+ * This program copies data from mongodb to postgresql
+ * 1. read environment variables for db connection
+ * 2. read environment for mongo url connection
+ * 3. read environment for configuration (table)
+ * 4. connect to mongodb
+ * 5. connect to psql
+ * 6. read from mongo
+ * 7. write to postgres
+ * 8. disconnect
+ * 9. disconnect
+ * 10. write report to disk
+ * */
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout << "it works\n";
+}
