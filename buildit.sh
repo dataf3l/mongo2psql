2c2a4172adb5ffcd0a68fa7a9136822926ed7640
Felipe@MBP2 <auditor400@gmail.com>
2018-07-12 18:22:23 -0500

    XCode Proj

--- buildit.sh
+++ buildit.sh
@@ -1,3 +1,3 @@
 # -static-libgcc -static-libstdc++
 # -static -static-libgcc -static-libstdc++
-g++ -std=c++11 -I. -I./lib/libpqxx/include  -o ./mongo2psql  mongo2psql.cpp -lpqxx -lpq  $(pkg-config --cflags --libs libmongocxx) -Wl,-rpath,/usr/local/lib 
+g++ -std=c++11 -I. -I./lib/libpqxx/include  -o ./mongo2psql  mongo2psql.cpp -lpqxx -lpq  $(pkg-config --cflags --libs libmongocxx) -Wl,-rpath,/usr/local/lib -static
